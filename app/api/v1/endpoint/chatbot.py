a33d1ece0c4f0f094cbbac9bb2d4017c39ab0b04
Nikhilj29 <nickkhj29@gmail.com>
2026-05-20 19:55:22 +0530

    embedding,vector storage and query

--- app/api/v1/endpoint/chatbot.py
+++ app/api/v1/endpoint/chatbot.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter, File,UploadFile
 from pypdf import PdfReader
+from sentence_transformers import SentenceTransformer
 import io
-import re
+import nltk
+import numpy as np
+import faiss
+
 router = APIRouter()
 
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+nltk.download('punkt', quiet=True)
 @router.post("/")
 async def postchat(query:str,file:UploadFile = File(...)):
     print(file,"file")
@@ -20,6 +28,30 @@ async def postchat(query:str,file:UploadFile = File(...)):
     
     print(text,"text")
     
-    extracting_text_array = re.split(r'[.|:]', text)
-    print(extracting_text_array,"array")
-    return "GOT STRING"
+    extracted_array = nltk.sent_tokenize(text)
+    print(extracted_array,"extracted array")
+    
+    embedding = model.encode(extracted_array)
+    
+    db_vector = np.array(embedding,dtype=np.float32)
+    dimention = db_vector.shape[1]
+    
+    index = faiss.IndexFlatL2(dimention)
+    
+    index.add(db_vector)    
+    query_array = [query]
+    query_embediing = model.encode(query_array)
+    
+    query_vector = np.array(query_embediing,dtype=np.float32)
+    
+    distance_of_sol,index_of_sol = index.search(query_vector,k=1)
+    
+    print(distance_of_sol,index_of_sol,"result")    
+    
+    best_match_index = index_of_sol[0][0]
+    
+    print(best_match_index,"best match index")
+    
+    print(extracted_array[best_match_index],"SOlution")
+    
+    return extracted_array[best_match_index]
